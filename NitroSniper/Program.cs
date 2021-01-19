9c01b2957ef37633c74cea4a6231135d3473ff06
Ven <sgtmattis@gmail.com>
2021-01-19 23:50:37 +0100

    Add project files.

---
+++ NitroSniper/Program.cs
@@ -0,0 +1,45 @@
+﻿using DSharpPlus;
+using Newtonsoft.Json;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace NitroSniper
+{
+    public class Program
+    {
+
+        private DiscordClient[] _clients;
+        private Config _config;
+        public static void Main(string[] args)
+        {
+            new Program(args).InitAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        public Program(string[] args)
+        {
+            _config = JsonConvert.DeserializeObject<Config>(File.ReadAllText("./config.json"));
+        }
+
+        public async Task InitAsync()
+        {
+            foreach (var slaveToken in _config.Slaves)
+            {
+                DiscordClient client;
+                client = new DiscordClient(new DiscordConfiguration() { Token = slaveToken, TokenType = TokenType.User });
+
+                await client.ConnectAsync();
+            }
+
+            await Task.Delay(-1);
+        }
+
+    }
+
+
+    public class Config
+    {
+        public string Master {get; set;}
+        public string UserAgent { get; set; }
+        public string Webhook { get; set; }
+        public System.Collections.Generic.IList<string> Slaves { get; set; }
+}
